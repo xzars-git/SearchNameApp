7a51b27496ef392ef1b0dd189cab292aeb4d80ab
Arsenius Purbandono <xzars@upi.edu>
2022-06-11 12:58:49 +0700

    Update

--- app/src/main/java/com/example/searchapp/Adapter/PersonAdapter.java
+++ app/src/main/java/com/example/searchapp/Adapter/PersonAdapter.java
@@ -1,12 +1,13 @@
 package com.example.searchapp.Adapter;
 
-import android.support.annotation.NonNull;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.searchapp.Model.Person;
 import com.example.searchapp.R;
@@ -22,15 +23,14 @@ public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.MyViewHold
     }
 
     @NonNull
-    @androidx.annotation.NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull @androidx.annotation.NonNull ViewGroup viewGroup, int i) {
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.person_layout, viewGroup, false);
         return new MyViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull @androidx.annotation.NonNull MyViewHolder myViewHolder, int i) {
+    public void onBindViewHolder(@NonNull  MyViewHolder myViewHolder, int i) {
         myViewHolder.text_name.setText(personList.get(i).getName());
         myViewHolder.text_email.setText(personList.get(i).getEmail());
         myViewHolder.text_PhoneNumber.setText(personList.get(i).getPhone());
@@ -46,7 +46,7 @@ public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.MyViewHold
         CardView root_view;
         TextView text_PhoneNumber, text_name, text_email;
 
-        public MyViewHolder(@NonNull @androidx.annotation.NonNull View itemView) {
+        public MyViewHolder(@NonNull  View itemView) {
             super(itemView);
             root_view = (CardView) itemView.findViewById(R.id.root_view);
             text_PhoneNumber = (TextView) itemView.findViewById(R.id.text_PhoneNumber);
--- app/src/main/java/com/example/searchapp/MainActivity.java
+++ app/src/main/java/com/example/searchapp/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.searchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.Toast;
